519eb574c9971329a7de1a2f51cea1f7dc18c8fc
codegeass25 <angelroy.longakit@deped.gov.ph>
2026-08-25 08:58:08 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='whitelabel-pwa-v5-8-0';
+const CACHE='whitelabel-pwa-v5-7-0';
 const APP_SHELL=['./','./index.html'];
 const isDynamicBranding=(url)=>url.pathname.startsWith('/api/')||url.pathname.startsWith('/branding/')||url.pathname.endsWith('/manifest.webmanifest');
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
